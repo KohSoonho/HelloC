81cd497b86121d3432c32d6fce9ac371d90d6898
Atsuhiro Kou <millerttime60@gmail.com>
2018-01-21 13:44:08 +0900

    add sample1
    add sample

---
+++ sample1.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main() {
+	int a = 15, b = 20; /*This is comment*/
+	
+	// C language is very difficult ???
+	
+	printf("a = 15\nb= 20\n");
+	printf("a + b = %d\n", a + b);
+	printf("push any key\n");
+	printf("End");
+	return 0;
+	}
