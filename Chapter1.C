1d46efff1fa5b5724355be757e722c9e1b16a813
Atsuhiro Kou <millerttime60@gmail.com>
2018-01-28 23:58:51 +0900

    Add new file
    This file is written Chapter1 of "Struggle C language"

---
+++ Chapter1.C
@@ -0,0 +1,14 @@
+/*
+ At first, I make program that do nothing.
+ C language are union of function.
+ These function are not orderd, but `main` function are done at first.
+ */
+
+int main(void) {
+    /* int = type, main = name of function, void = argument(this mean no arguments) */
+	return 0;
+    /*
+     inside {} function body is written
+     At C, to finish function I must write return sentense
+     */
+	}
